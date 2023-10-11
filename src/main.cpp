49cd0b09f51ae2f31c70e6612c43000a2b2e9baf
tigerRose <fuxiang_zhao@163.com>
2023-10-11 15:45:09 +0800

    wrap the source program into a python library

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,42 @@
-/* Copyright (C) BGI-Reasearch - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by STOmics development team P_stomics_dev@genomics.cn, 2023
- */
+#include <pybind11/pybind11.h>
 
 #include "pipeline.h"
-#include "timer.h"
 
-#include <iostream>
 #include <string>
-#include <thread>
 using namespace std;
 
-#include <CLI/CLI.hpp>
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-constexpr auto APP_NAME    = "cusingler";
-constexpr auto APP_VERSION = "1.0";
+namespace py = pybind11;
 
-int main(int argc, char** argv)
+void cusingler(string& ref_h5, string& qry_h5, string& stat_file, int cores, int gpuid)
 {
-    // Parse the command line parameters.
-    CLI::App app{ string(APP_NAME) + ": Accelerate singleR with GPU." };
-    app.footer(string(APP_NAME) + " version: " + APP_VERSION);
-    app.get_formatter()->column_width(40);
-
-    // Required parameters
-    string ref_h5, qry_h5, stat_file;
-    app.add_option("-r,--ref", ref_h5, "Reference h5 file")
-        ->check(CLI::ExistingFile)
-        ->required();
-    app.add_option("-q,--qry", qry_h5, "Query h5 file")
-        ->check(CLI::ExistingFile)
-        ->required();
-    app.add_option("-o,--out", stat_file, "Output stat file, seperated by tab")
-        ->required();
-
-    // Optional parameters
-    int cores = std::thread::hardware_concurrency();
-    app.add_option("-c", cores, "CPU core number, default detect")
-        ->check(CLI::PositiveNumber);
-    int gpuid = 0;
-    app.add_option("-g", gpuid, "Set gpu id, default 0")->check(CLI::NonNegativeNumber);
-
-    CLI11_PARSE(app, argc, argv);
-
-    Timer    timer;
     Pipeline pipeline = Pipeline(ref_h5, qry_h5, stat_file, cores, gpuid);
     pipeline.train();
     pipeline.score();
     pipeline.finetune();
     pipeline.dump();
-    cout << "Total cost time(s): " << timer.toc() << endl;
+}
 
-    return 0;
+PYBIND11_MODULE(cusingler, m) {
+    m.doc() = R"pbdoc(
+        cell annotation accelerated by GPU
+        -----------------------
+
+        .. currentmodule:: cusingler
+
+        .. autosummary::
+           :toctree: _generate
+
+           cusingler
+    )pbdoc";
+
+    m.def("cusingler", &cusingler, "cusingler function");
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
 }
